1c4a18a9f15c486264e364b2597b7ca8a4089df1
iamtalhaarifvlogs <iamtalhaarifvlogs@gmail.com>
2026-01-11 04:40:26 +0500

    Update FacebookButton.tsx

--- app/components/FacebookButton.tsx
+++ app/components/FacebookButton.tsx
@@ -10,16 +10,16 @@ export default function FacebookButton() {
       <Link
         href={facebookUrl}
         target="_blank"
+        aria-label="Facebook"
         className="
-          group
           flex items-center justify-center
           w-14 h-14
           bg-[#1877F2] hover:bg-[#145dbf]
           text-white
           rounded-full
           shadow-xl
-          transition-all duration-300
-          overflow-hidden
+          transition-transform duration-300
+          hover:scale-105
         "
       >
         {/* Facebook Icon */}
@@ -31,23 +31,6 @@ export default function FacebookButton() {
         >
           <path d="M22.675 0h-21.35C.597 0 0 .597 0 1.326v21.348C0 23.403.597 24 1.326 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.894-4.788 4.66-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.796.715-1.796 1.763v2.31h3.587l-.467 3.622h-3.12V24h6.116C23.403 24 24 23.403 24 22.674V1.326C24 .597 23.403 0 22.675 0z" />
         </svg>
-
-        {/* Hover Text */}
-        <span
-          className="
-            absolute left-full ml-3
-            max-w-0 group-hover:max-w-xs
-            overflow-hidden whitespace-nowrap
-            bg-[#1877F2]
-            text-white text-sm font-medium
-            px-4 py-2
-            rounded-full
-            shadow-lg
-            transition-all duration-300
-          "
-        >
-          Follow us on Facebook
-        </span>
       </Link>
     </div>
   );
